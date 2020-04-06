aa1bde0675e7237964adc26982fa791243372db7
dtl1 <61438998+dtl1@users.noreply.github.com>
2020-04-06 15:02:26 +0100

    javadoc comments

--- ElizaGUI.java
+++ ElizaGUI.java
@@ -1,5 +1,9 @@
 public class ElizaGUI {
 
+    /**
+     * creates a gui object and then calls the methods to create all gui elements and then show the main menu
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         GUI gui = new GUI();
         gui.createGUI();
